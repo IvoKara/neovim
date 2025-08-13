ec8ee94e52811acdcd00c1ee75a4b4261c529091
IvoKara <ivo.i.karaneshev@gmail.com>
2025-08-13 13:36:26 +0300

    feat(options): move options from .vimrc to lua options file

---
+++ lua/config/options.lua
@@ -0,0 +1,34 @@
+-- Options converted from ~/.vimrc
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.compatible = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.backup = false
+vim.opt.scrolloff = 10
+vim.opt.wrap = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.hlsearch = true
+vim.opt.history = 1000
+
+vim.opt.magic = true
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = "list:longest"
+vim.opt.wildignore = "*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx"
+
+-- Enable filetype detection, plugins, and indentation
+vim.cmd('filetype on')
+vim.cmd('filetype plugin on') 
+vim.cmd('filetype indent on')
+
+-- Enable syntax highlighting
+vim.cmd('syntax on')
+vim.cmd('syntax enable')
