ee8a98e63454800b2fcc2d535cf7bd55ff66aba8
IvoKara <ivo.i.karaneshev@gmail.com>
2025-08-25 10:50:52 +0300

    update identation configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.compatible = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.autoindent = true
-vim.opt.expandtab = true
 vim.opt.backup = false
 vim.opt.scrolloff = 10
 vim.opt.wrap = false
@@ -25,10 +26,11 @@ vim.opt.wildmode = "list:longest"
 vim.opt.wildignore = "*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx"
 
 -- Enable filetype detection, plugins, and indentation
-vim.cmd('filetype on')
-vim.cmd('filetype plugin on') 
-vim.cmd('filetype indent on')
+vim.cmd("filetype on")
+vim.cmd("filetype plugin on")
+vim.cmd("filetype indent on")
 
 -- Enable syntax highlighting
-vim.cmd('syntax on')
-vim.cmd('syntax enable')
+vim.cmd("syntax on")
+vim.cmd("syntax enable")
+
